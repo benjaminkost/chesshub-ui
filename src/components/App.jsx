99795c5f2fdfd3c86b7721bbfea3e060688c01d6
Ben Kostka <mail@ben-kostka.de>
2025-04-16 14:44:49 +0200

    Add Login component and integrate it into routing
    Introduce a Login component to handle user authentication with email/username and password inputs. Update `App.jsx` to include routes for `/auth/login` and `/auth/signin` linked to the new Login component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import '../styles/App.css'
 import { Registration } from "./Registration";
 import {BrowserRouter, Route, Routes} from "react-router";
+import { Login } from "./Login";
 
 export function App() {
 
@@ -11,6 +12,8 @@ export function App() {
                   <Routes>
                       <Route path="/auth/register" element={< Registration />} />
                       <Route path="/auth/signup" element={< Registration />} />
+                      <Route path="/auth/login" element={< Login />} />
+                      <Route path="/auth/signin" element={< Login />} />
                   </Routes>
               </main>
           </BrowserRouter>
