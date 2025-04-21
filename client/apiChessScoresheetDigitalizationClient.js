4d0a24d180265d0db2bafc5447f752d533dd8365
Ben Kostka <mail@ben-kostka.de>
2025-04-21 23:22:01 +0200

    Add environment-based URLs and new API client
    Replaced hardcoded base URL with environment variables for better flexibility and added `.env.sample` for reference. Introduced a new API client for scoresheet digitalization and updated dependencies to include `form-data`.

---
+++ client/apiChessScoresheetDigitalizationClient.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+const baseURL = import.meta.env.VITE_CHESS_SCORESHEET_DIGITALIZATION_BASEURL;
+
+const apiClient = axios.create({
+    baseURL: baseURL,
+    timeout: 10000
+})
+
+export const _post = (url, config = {}) => {
+    try {
+        return apiClient.post(url, config)
+            .then(res => {
+            console.log(res)
+        })
+            .catch(err => {
+                console.log(err)
+            });
+    } catch (e) {
+        console.error("Error when sending: " + e)
+    }
+}
