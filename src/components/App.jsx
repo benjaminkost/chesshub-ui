0cd5f25d7946434a3d9d42bf1ac39a04c196aa74
Ben Kostka <mail@ben-kostka.de>
2025-04-22 00:23:02 +0200

    Add UploadImage component and route to handle image upload
    Introduce a new `UploadImage` component with file upload functionality. Updated `App.jsx` to include a route for accessing the image upload feature. This enables users to select and upload files, with basic validation and API integration.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import '../styles/App.css'
 import { Registration } from "./Registration";
 import {BrowserRouter, Route, Routes} from "react-router";
 import { Login } from "./Login";
+import {UploadImage} from "./UploadImage.jsx";
 
 export function App() {
 
@@ -14,6 +15,7 @@ export function App() {
                       <Route path="/auth/signup" element={< Registration />} />
                       <Route path="/auth/login" element={< Login />} />
                       <Route path="/auth/signin" element={< Login />} />
+                      <Route path="/uploadImage" element={< UploadImage />} />
                   </Routes>
               </main>
           </BrowserRouter>
