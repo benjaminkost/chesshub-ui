2eccb2f35540be623f6dc3efa8f3a242b1ef55d1
Ben Kostka <mail@ben-kostka.de>
2025-05-22 17:34:49 +0200

    Add Font Awesome CSS file to project
    This adds the Font Awesome Free 5.15.4 CSS file to the assets directory. It will enable the use of Font Awesome icons throughout the project. The file is licensed under a combination of CC BY 4.0, SIL OFL 1.1, and MIT.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,29 @@ export function Home() {
 
     return (
         <>
-            <h1>ChessHub</h1>
-            <nav>
+            <header id="header">
+                <h1><a href="/">ChessHub</a></h1>
+                <nav className="main">
+                    <ul>
+                        <li className="search">
+                            <a className="fa-search">Search</a>
+                            <form id="search" method="get" action="#">
+                                <input type="text" name="query" placeholder="Search" />
+                            </form>
+                        </li>
+                        <li className="menu">
+                            <a className="fa-bars">Menu</a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <article className="post">
+                <header>
+                    <div className="title">
+                        <h1>ChessHub Home</h1>
+                    </div>
+                </header>
+            <nav className="links">
                 <ul>
                     <li>
                         <Link to = "/auth/register">Register</Link>
@@ -19,6 +40,7 @@ export function Home() {
                     </li>
                 </ul>
             </nav>
+            </article>
         </>
     )
 }
