67cc54649862784f721ca02ce8e278fadf422ad6
Ben Kostka <mail@ben-kostka.de>
2025-12-03 10:23:07 +0100

    feature-30-create-header-component:

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,31 +2,19 @@ import {Link} from "react-router-dom";
 
 export function Header() {
     return (
-        <header id="header">
-            <h1><a href="/">ChessHub</a></h1>
-            <nav className="links">
-                <ul>
-                    <li>
-                        <Link to = "/auth/register">Register</Link>
-                    </li>
-                    <li>
-                        <Link to = "/uploadImage">Upload Image</Link>
-                    </li>
-                </ul>
-            </nav>
-            <nav className="main">
-                <ul>
-                    <li className="search">
-                        <a className="fa-search">Search</a>
-                        <form id="search" method="get" action="#">
-                            <input type="text" name="query" placeholder="Search" />
-                        </form>
-                    </li>
-                    <li className="menu">
-                        <a className="fa-bars">Menu</a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <>
+            <button id={"menuButton"}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+            <div id={"homePageButton"}>
+                <img src={"src/assets/images/black_pawn.png"} alt={"Black Pawn"}/>
+                <h1>ChessHub</h1>
+            </div>
+            <br/>
+            <br/>
+
+        </>
     )
 }
