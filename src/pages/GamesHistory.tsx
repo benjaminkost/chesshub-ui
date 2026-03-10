94af927ffe5ac586bb3350fe94470f9f817efbbc
Ben Kostka <mail@ben-kostka.de>
2026-03-10 14:59:58 +0100

    feature-38-history-of-games: Add spacing for footer

--- src/pages/GamesHistory.tsx
+++ src/pages/GamesHistory.tsx
@@ -2,14 +2,20 @@ import {GamesTable} from "../components/GamesTable.js";
 import {defaultGamesTableData} from "../components/GamesTable.js";
 import {Header} from "../components/Header.js";
 import Footer from "../components/Footer.js";
+import { Box } from "@mui/material";
 
 function GamesHistory() {
-
 
     return (
         <>
             <Header loggedIn={true} />
             <GamesTable rows={defaultGamesTableData}/>
+            <Box
+                sx={{
+                    flexGrow: 1,
+                    minHeight: "48vh"
+                }}
+            />
             <Footer/>
         </>
     )
