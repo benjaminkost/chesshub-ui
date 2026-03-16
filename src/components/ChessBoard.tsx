9a28f87ca58a7ec86742f7e1b496259d41e408b6
Ben Kostka <mail@ben-kostka.de>
2026-03-16 18:52:43 +0000

    feature-37-add-page-to-view-single-games: Add chess logic with chess.js + beginnings of creating pgn viewer

--- src/components/ChessBoard.tsx
+++ src/components/ChessBoard.tsx
@@ -5,25 +5,43 @@ import {Config} from "@lichess-org/chessground/config";
 import "@lichess-org/chessground/assets/chessground.base.css";
 import "@lichess-org/chessground/assets/chessground.brown.css";
 import "@lichess-org/chessground/assets/chessground.cburnett.css";
+import {Chess} from "chess.js";
 
 interface ChessBoardProps{
     heightWidth?: number;
     contained?: boolean;
     config?: Config;
+    pgn?: string;
 }
 
-export default function ChessBoard({heightWidth=600, contained=false, config={}}: ChessBoardProps) {
+export default function ChessBoard({heightWidth=600, contained=false, config={}, pgn=""}: ChessBoardProps) {
     const ref = React.useRef<HTMLDivElement>(null);
     const [api, setApi] = React.useState<Api | null>(null);
 
     React.useEffect(() => {
-        if (ref && ref.current && !api){
-            const chessGround = Chessground(ref.current,
-                {animation: {enabled: true, duration: 200}
-                ,...config});
-            setApi(chessGround);
-        } else if(ref && ref.current && api){
-            api.set(config);
+        try{
+            const chess = new Chess();
+
+            chess.loadPgn(pgn);
+            const history = chess.history({verbose: true});
+            const lastMove = history.length > 0 ? [history[history.length-1].from, history[history.length-1].to] : undefined;
+
+            const config = {
+                fen: chess.fen(),
+                lastMove: lastMove,
+                viewOnly: true
+            };
+
+            if (ref.current && !api){
+                const chessGround = Chessground(ref.current,
+                    {animation: {enabled: true, duration: 200}
+                        ,...config});
+                setApi(chessGround);
+            } else if(ref.current && api){
+                api.set(config);
+            }
+        } catch (e){
+            console.log("Error in pgn: ", e);
         }
     }, [ref]);
 
@@ -32,7 +50,7 @@ export default function ChessBoard({heightWidth=600, contained=false, config={}}
     }, [api, config]);
 
     return (
-        <div className={"cg-board-wrap"} style={{height: contained ? '100%' : heightWidth, width: contained ? '100%' : heightWidth}}>
+        <div style={{height: contained ? '100%' : heightWidth, width: contained ? '100%' : heightWidth}}>
             <div ref={ref} style={{height: '100%', width: '100%', display: 'table'}}/>
         </div>
     );
