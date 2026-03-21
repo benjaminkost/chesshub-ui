46c921eb2793979b1f95106afa90cc32f6da3e06
Ben Kostka <mail@ben-kostka.de>
2026-03-21 01:26:13 +0000

    feature-47-club-management-page: PART 2: NON TICKET RELATED: updated the way pages with header and footer are shown -> added component PageLayout.tsx to handle white spacing under content of page

--- src/pages/TeamManagement.tsx
+++ src/pages/TeamManagement.tsx
@@ -1,24 +1,12 @@
-import {Header} from "@/components/Header";
 import {allUsers, dummyTeam} from "@/dummyData";
-import {Box} from "@mui/material";
-import Footer from "@/components/Footer";
 import TeamManagementTable from "@/components/TeamManagementTable";
+import PageLayout from "@/components/PageLayout";
 
 export default function TeamManagement(){
     return (
-        <>
-            <Header loggedIn={true} />
-            <TeamManagementTable
-                allUsers={allUsers}
-                team={dummyTeam}
-            />
-            <Box
-                sx={{
-                    flexGrow: 1,
-                    minHeight: "30vh"
-                }}
-            />
-            <Footer/>
-        </>
-    )
+        <PageLayout loggedIn={true} children={<TeamManagementTable
+            allUsers={allUsers}
+            team={dummyTeam}
+        />} />
+    );
 }
--- src/pages/ViewSingleGame.tsx
+++ src/pages/ViewSingleGame.tsx
@@ -1,24 +1,10 @@
-import {Header} from "@/components/Header";
 import React from "react";
-import Footer from "@/components/Footer";
-import { Box } from "@mui/material";
 import GameAnalyzer from "@/components/GameAnalyzer";
 import {dummyPgn} from "@/dummyData";
+import PageLayout from "@/components/PageLayout";
 
 export default function ViewSingleGame() {
     return (
-        <>
-            <Header loggedIn={true} />
-            <Box sx={{
-                flexGrow: 1,
-                minHeight: "3vh"
-            }}/>
-            <GameAnalyzer pgn={dummyPgn} />
-            <Box sx={{
-                flexGrow: 1,
-                minHeight: "3.5vh"
-            }}/>
-            <Footer />
-        </>
+        <PageLayout loggedIn={true} children={<GameAnalyzer pgn={dummyPgn} />} />
     );
 }
