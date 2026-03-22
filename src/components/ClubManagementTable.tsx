ed991691c7ca1a93e5629027212f8a2ccd4ace63
Ben Kostka <mail@ben-kostka.de>
2026-03-22 17:50:15 +0000

    feature-47-club-management-page: Add Popup dialog field to add teams to club

--- src/components/ClubManagementTable.tsx
+++ src/components/ClubManagementTable.tsx
@@ -3,16 +3,22 @@ import {DataGrid, GridColDef} from "@mui/x-data-grid";
 import React from "react";
 import {ClubTeams} from "@/types/club";
 import {Team} from "@/types/team";
-import {Member} from "@/types/user";
+import {Member, User} from "@/types/user";
 import {useNavigate} from "react-router-dom";
+import {AddTeamPopupDialog} from "@/components/AddTeamPopupDialog";
 
 interface ClubManagementTable{
     club: ClubTeams;
+    allUsers: User[];
 }
 
-export default function ClubManagementTable({club}: ClubManagementTable){
+export default function ClubManagementTable({club, allUsers}: ClubManagementTable){
     const [rows, setRows] = React.useState<Team[]>(club.teams);
     const navigate = useNavigate();
+
+    const addTeam = (newTeam:Team) => {
+        setRows((prevState) => [...prevState, newTeam]);
+    }
 
     const navigateToTeam = () => {
         navigate("/team-management");
@@ -21,10 +27,19 @@ export default function ClubManagementTable({club}: ClubManagementTable){
     const columns = React.useMemo<GridColDef[]>(() => [
         {field: "id", headerName: "ID", resizable: false, flex: 1},
         {field: "name", headerName: "Mannschaft", resizable: false, flex: 3, renderCell: (params) => {
-            return (<Box sx={{"&:hover": {color: "blue", cursor: "pointer"}}} onClick={navigateToTeam}>{params.value}</Box>)
+            return (
+                <Box
+                    sx={{"&:hover":
+                            {
+                                color: "blue",
+                                cursor: "pointer"}
+                        }
+                    }
+                    onClick={navigateToTeam}>
+                    {params.value}
+                </Box>)
             }},
-        {field: "admin", headerName: "Mannschaftsleiter", resizable: false, flex: 3, valueFormatter: (value:Member, row) => value.name
-        }
+        {field: "admin", headerName: "Mannschaftsleiter", resizable: false, flex: 3, valueFormatter: (value:Member, row) => value.name}
     ],[rows]);
 
     return (
@@ -42,6 +57,12 @@ export default function ClubManagementTable({club}: ClubManagementTable){
                 }}
                 columns={columns}
                 rows={rows}
+                slots={{
+                    footer: () => < AddTeamPopupDialog
+                                                        allUsers={allUsers}
+                                                       addTeam={addTeam}
+                                                       currentCountOfRows={rows.length}/>
+                }}
             />
         </Paper>
     )
