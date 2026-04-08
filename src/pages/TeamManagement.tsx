b50e0b48a48e7d4d148349a3c9c9665603fd9c44
Ben Kostka <mail@ben-kostka.de>
2026-04-08 21:46:54 +0200

    Refactor: Rename variables

--- src/pages/TeamManagement.tsx
+++ src/pages/TeamManagement.tsx
@@ -1,12 +1,12 @@
-import {allMembers, dummyTeam} from "@/dummyData";
+import {allUsers, dummyTeamVm} from "@/dummyData";
 import TeamManagementTable from "@/components/TeamManagementTable";
 import PageLayout from "@/components/PageLayout";
 
 export default function TeamManagement(){
     return (
         <PageLayout loggedIn={true} children={<TeamManagementTable
-            allUsers={allMembers}
-            team={dummyTeam}
+            allUsers={allUsers}
+            team={dummyTeamVm}
         />} />
     );
 }
