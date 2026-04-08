2e2739c2462afe1e1e9a5ddab8a68632227558e9
Ben Kostka <mail@ben-kostka.de>
2026-04-08 21:49:10 +0200

    Refactor: Rename type

--- src/components/ProfileSettings.tsx
+++ src/components/ProfileSettings.tsx
@@ -1,9 +1,9 @@
 import {Alert, Box, Button, Snackbar, SnackbarCloseReason, TextField, Typography} from "@mui/material";
 import React from "react";
-import {User} from "@/types/user";
+import {UserModel} from "@/types/models/user.model";
 
 interface ProfileSettingsProps {
-    user: User
+    user: UserModel
 }
 
 export function ProfileSettings({user}:ProfileSettingsProps) {
