32820ce1cd9a428b15a1c1bc4b7f2b8044afc992
Ben Kostka <mail@ben-kostka.de>
2026-04-13 01:37:33 +0200

    Rollback change I did before for the ChessBoardEditor.tsx so an object for metadata is passed

--- src/components/ChessBoardEditor.tsx
+++ src/components/ChessBoardEditor.tsx
@@ -1,76 +1,54 @@
-import {Box, Button, Grid} from "@mui/material";
-import {SmartChessBoard} from "@/components/ChessBoard";
+import { Box, Button, Grid } from "@mui/material";
+import { SmartChessBoard } from "@/components/ChessBoard";
 import MoveList from "@/components/MoveList";
-import React, {Key} from "react";
-import {Api} from "@lichess-org/chessground/api";
-import {Chess, Move} from "chess.js";
-import {MetaDataForGameInput} from "@/components/MetaDataForGameInput";
-import {gamesApi} from "@/api/chesshub";
-import {useNavigate} from "react-router-dom";
+import React, { Key } from "react";
+import { Api } from "@lichess-org/chessground/api";
+import { Chess, Move } from "chess.js";
+import { MetaDataForGameInput } from "@/components/MetaDataForGameInput";
+import { _post } from "../../bff/src/clients/apiChessHubCoreClient";
+import { useNavigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
-import {Dayjs} from "dayjs";
-import {produce} from "immer";
-import { Team, User, GameRequest } from "@benaurel/chesshub-core-client";
-import { ROUTES } from "@/routes";
+import { parsePgnToGameState } from "../../bff/src/utils/pgnParsing"
+import { GameState, GameStateNode } from "@/types/models/game.model";
+import { produce } from "immer";
+import { GameMetaData, GameWithTeamVm } from "@/types/viewmodels/game.vm";
+import { mapGameWithTeamVmToGameMetaData } from "../../bff/src/mapper/game.mapper";
+import { TeamSimpleVm } from "@/types/viewmodels/team.vm";
+import {ROUTES} from "@/routes";
+import {useAuth} from "@/context/AuthContext";
+import {convertGameStateToPgn} from "../../bff/src/utils/interactWithGameState";
 
 export interface ChessBoardEditorProps {
-    allTeams: Team[];
-    user: User;
-    initialWhitePlayer?: string;
-    initialBlackPlayer?: string;
-    initialDate?: Dayjs | null;
-    initialEvent?: string;
-    initialRound?: number;
-    initialTeam?: Team | undefined;
-    initialMoves?: string;
+    allTeams: TeamSimpleVm[];
+    game: GameWithTeamVm;
 }
 
-const defaultValues = {
-    initialWhitePlayer: "",
-    initialBlackPlayer: "",
-    initialDate: null,
-    initialEvent: "",
-    initialRound: undefined,
-    initialTeam: undefined,
-    initialMoves: "",
-}
-
-export function ChessBoardEditor({allTeams,
-                                     user,
-                                     initialWhitePlayer=defaultValues.initialWhitePlayer,
-                                     initialBlackPlayer=defaultValues.initialBlackPlayer,
-                                     initialDate=defaultValues.initialDate,
-                                     initialEvent=defaultValues.initialEvent,
-                                     initialRound=defaultValues.initialRound,
-                                     initialTeam=defaultValues.initialTeam,
-                                     initialMoves=defaultValues.initialMoves
-                                 }:ChessBoardEditorProps){
+export function ChessBoardEditor({ allTeams, game }: ChessBoardEditorProps) {
     const [chessApi, setChessApi] = React.useState<Api | null>(null);
     const [lastMove, setLastMove] = React.useState<Key[] | undefined>();
-    const [gameState, setGameState] = React.useState<GameState>(parsePgnToGameState(initialMoves));
-    const [whitePlayer, setWhitePlayer] = React.useState<string>(initialWhitePlayer);
-    const [blackPlayer, setBlackPlayer] = React.useState<string>(initialBlackPlayer);
-    const [date, setDate] = React.useState<Dayjs | null>(initialDate);
-    const [event, setEvent] = React.useState<string>(initialEvent);
-    const [round, setRound] = React.useState<number | undefined>(initialRound);
-    const [team, setTeam] = React.useState<Team | undefined>(initialTeam);
+    const [gameState, setGameState] = React.useState<GameState>(parsePgnToGameState(game.moves));
+    const [gameMetaData, setGameMetaData] = React.useState<GameMetaData>(mapGameWithTeamVmToGameMetaData(game));
+    const { user } = useAuth();
     const navigate = useNavigate();
 
-    const convertTreeToPGNMoves = (): string => initialMoves || "";
-
     React.useEffect(() => {
-        if (!lastMove) return;
         const chess = new Chess(gameState.allGameStates[gameState.activeStateId].fen);
         try {
-            const move:Move = chess.move({from: lastMove[0].toString(), to: lastMove[1].toString()});
-            chessApi?.set({fen: chess.fen()});
+            if (lastMove?.length !== 2) throw Error("Move given from chessboard is no completet move");
+            const move: Move = chess.move({ from: lastMove[0].toString(), to: lastMove[1].toString() });
+            chessApi?.set({ fen: chess.fen() });
             const parentId = gameState.activeStateId;
 
             setGameState(prev => {
                 const parentState = prev.allGameStates[parentId];
-                const existingMoveId = parentState.nextMoves.find(id => prev.allGameStates[id].notation === move.san);
 
-                if (existingMoveId) return {...prev, activeStateId: existingMoveId};
+                const existingMoveId = parentState.nextMoves.find(id => (
+                    prev.allGameStates[id].notation === move.san
+                ));
+
+                if (existingMoveId) {
+                    return { ...prev, activeStateId: existingMoveId };
+                }
 
                 const newStateId = uuidv4();
                 const newState: GameStateNode = {
@@ -88,89 +66,114 @@ export function ChessBoardEditor({allTeams,
                     activeStateId: newStateId,
                     allGameStates: {
                         ...prev.allGameStates,
-                        [parentId]: { ...prev.allGameStates[parentId], nextMoves: [...prev.allGameStates[parentId].nextMoves, newStateId] },
+                        [parentId]: {
+                            ...prev.allGameStates[parentId],
+                            nextMoves: [...prev.allGameStates[parentId].nextMoves, newStateId]
+                        },
                         [newStateId]: newState
                     }
                 }
             });
-        } catch(e){
-            chessApi?.set({fen: chess.fen()});
+        } catch (e) {
+            chessApi?.set({ fen: chess.fen() });
         }
     }, [lastMove]);
 
     React.useEffect(() => {
-        chessApi?.set({fen: gameState.allGameStates[gameState.activeStateId].fen});
-    }, [gameState.activeStateId, chessApi]);
+        chessApi?.set({ fen: gameState.allGameStates[gameState.activeStateId].fen });
+    }, [gameState.activeStateId]);
 
     const handleSaveGame = async () => {
-        const gameRequest: GameRequest = {
-            whitePlayerName: whitePlayer,
-            blackPlayerName: blackPlayer,
-            moves: convertTreeToPGNMoves(),
-            event: event,
-            date: date ? date.format("YYYY-MM-DD") : undefined,
-            teamId: team?.id
+        const payload = {
+            "white_player_name": gameMetaData.whitePlayerName,
+            "black_player_name": gameMetaData.blackPlayerName,
+            "moves": convertGameStateToPgn(gameState),
+            "event": gameMetaData.event,
+            "date": gameMetaData.date?.toISOString(),
+            "team": gameMetaData.teamId
         };
-        
-        try {
-            await gamesApi.createGame(gameRequest);
-            navigate(ROUTES.GAMES.LIST_USER.func(user.id!));
-        } catch (error) {
-            console.error("Failed to save game:", error);
-            alert("Fehler beim Speichern der Partie.");
+        await _post("", payload);
+
+        if (!user){
+            navigate(ROUTES.AUTH.LOGIN.func());
+            return;
         }
+        navigate(ROUTES.GAMES.LIST_USER.func(user.id));
     }
 
     const handleMoveSelect = (id: string) => {
-        setGameState(prev => ({ ...prev, activeStateId: id }));
+        setGameState(prev => ({
+            ...prev,
+            activeStateId: id
+        }));
     };
 
-    const handleEvaluationChange = (id:string, evaluation: number | null) => {
-        setGameState(produce((prev:GameState) => {
+    const handleEvaluationChange = (id: string, evaluation: number | null) => {
+        setGameState(produce((prev: GameState) => {
             const node = prev.allGameStates[id];
             if (!node) return prev;
-            if (!node.analysis) node.analysis = { recommendedMoves: [], depth: 0, eval: { centiPawn: 0, isMate: false, mateIn: 0} };
+
+            if (!node.analysis) {
+                node.analysis = {
+                    recommendedMoves: [],
+                    depth: 0,
+                    eval: { centiPawn: 0, isMate: false, mateIn: 0 }
+                }
+            }
+
             node.analysis.eval.centiPawn = evaluation ?? 0;
         }))
     }
 
+    const onChangeGameMetaData = (update: Partial<GameMetaData>) => {
+        setGameMetaData(prev => ({ ...prev, ...update }));
+    }
+
     return (
-        <Box sx={{ display: "flex", flexDirection: "column", m: 2 }}>
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                m: 2
+            }}
+        >
             <Grid container>
                 <Grid size={2}></Grid>
                 <Grid size={8}>
-                    <Box sx={{ display: "flex", flexDirection: "row", mb: 2 }}>
-                        <SmartChessBoard setChessApi={setChessApi} setLastMove={setLastMove} config={{fen: gameState.allGameStates[gameState.activeStateId].fen}}/>
-                        <MoveList gameState={gameState} onMoveSelect={handleMoveSelect} setEvaluation={handleEvaluationChange} />
+                    <Box
+                        sx={{
+                            display: "flex",
+                            flexDirection: "row",
+                            mb: 2
+                        }}
+                    >
+                        <SmartChessBoard setChessApi={setChessApi} setLastMove={setLastMove} config={{ fen: gameState.allGameStates[gameState.activeStateId].fen }} />
+                        <MoveList
+                            gameState={gameState}
+                            onMoveSelect={handleMoveSelect}
+                            setEvaluation={handleEvaluationChange}
+                        />
                     </Box>
                 </Grid>
                 <Grid size={2}></Grid>
                 <Grid size={2}></Grid>
                 <Grid size={8}>
                     <MetaDataForGameInput
-                        allUsers={[]}
-                        whitePlayerData={whitePlayer}
-                        setWhitePlayer={setWhitePlayer as any}
-                        blackPlayerData={blackPlayer}
-                        setBlackPlayer={setBlackPlayer as any}
-                        dateData={date}
-                        setDate={setDate}
-                        eventData={event}
-                        setEvent={setEvent}
-                        roundData={round}
-                        setRound={setRound}
-                        team={team as any}
-                        setTeam={setTeam as any}
-                        allTeams={allTeams as any}
-                        user={user as any}
+                        allTeams={allTeams}
+                        gameMetaData={gameMetaData}
+                        onChangeGameMetaData={onChangeGameMetaData}
                     />
                 </Grid>
                 <Grid size={2}></Grid>
                 <Grid size={2}></Grid>
                 <Grid size={8}>
-                    <Button fullWidth onClick={handleSaveGame} sx={{ width: 600, mt: 2, backgroundColor: "gray", color: "white" }}>
-                        Partie speichern
-                    </Button>
+                    <Button fullWidth onClick={handleSaveGame} sx={{
+                        width: 600,
+                        mt: 2,
+                        backgroundColor: "gray",
+                        color: "white"
+                    }}
+                    >Partie speichern</Button>
                 </Grid>
                 <Grid size={2}></Grid>
             </Grid>
