bea09db7c4fed758891d396474926a8f7a4890fc
Ben Kostka <mail@ben-kostka.de>
2026-04-24 19:30:31 +0200

    Delete unused packages for generating types from openapi

--- openapi-ts.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@hey-api/openapi-ts';
-
-export default defineConfig({
-  input: 'https://raw.githubusercontent.com/benjaminkost/chesshub-core/feature/49/write-openapi-specification/api/src/main/resources/openapi.yaml',
-  output: 'src/api/generated',
-  plugins: [
-    '@hey-api/typescript',
-  ],
-});
